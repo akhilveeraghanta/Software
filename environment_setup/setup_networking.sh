352eb5376842353790e280e46f30cea6eb0d48f6
akhilveeraghanta <akhil.veeraghanta@outlook.com>
2020-06-24 09:31:32 -0700

    wip remoove bridge

--- environment_setup/setup_networking.sh
+++ environment_setup/setup_networking.sh
@@ -11,26 +11,18 @@ echo "================================================================"
 echo " Networking Interface Setup"
 echo "================================================================"
 
-TBOTS_WIFI_INTERFACE=tbots_wifi
-TBOTS_ETHERNET_INTERFACE=tbots_eth
-
-echo "Removing tbots network interfaces, if they exist"
-sudo ip link set dev $TBOTS_WIFI_INTERFACE down
-sudo ip link set dev $TBOTS_ETHERNET_INTERFACE down
-sudo brctl delbr $TBOTS_WIFI_INTERFACE
-sudo brctl delbr $TBOTS_ETHERNET_INTERFACE
-
 # get the "suggested" interface(s). The first interface is usually the correct
 # one but we provide the option to select other interfaces, which is useful for VM users
 SUGGESTED_INTERFACE=$(route | grep '^default' | grep -o '[^ ]*$')
 
 PS3="Suggested interfaces(s):
 $SUGGESTED_INTERFACE
-Select wifi interface (input 0 to skip): "
+> Select wifi interface (input 0 to skip): "
 
-echo "==================================================="
-echo " Configure WiFi interface ($TBOTS_WIFI_INTERFACE)"
-echo "==================================================="
+# remove previous profile if it exists
+rm ../src/.tbots_network_profile 2> /dev/null
+
+echo "> Configure WiFi interface"
 
 select INTERFACE in $(ls /sys/class/net);
 do
@@ -41,21 +33,17 @@ do
             ;;
         *)
             echo "> Selected interface $INTERFACE ($REPLY)"
-            sudo brctl addbr $TBOTS_WIFI_INTERFACE && \
-            sudo brctl addif $TBOTS_WIFI_INTERFACE $INTERFACE && \
-            sudo ip link set dev $TBOTS_WIFI_INTERFACE up && \
+            echo "TBOTS_WIFI_INTERFACE=$INTERFACE" >> ../src/.tbots_network_profile
             echo "> WiFi Interface Configured"
             break
             ;;
     esac
 done
 
-PS3="Select ethernet interface (input 0 to skip): "
+PS3="> Select ethernet interface (input 0 to skip): "
 
-echo "==================================================="
-echo " Configure Ethernet interface ($TBOTS_ETHERNET_INTERFACE)"
-echo "==================================================="
-select INTERFACE in $(ls -I $TBOTS_WIFI_INTERFACE /sys/class/net );
+echo "> Configure Ethernet interface"
+select INTERFACE in $(ls /sys/class/net);
 do
     case $INTERFACE in
         "")
@@ -64,11 +52,12 @@ do
             ;;
         *)
             echo "> Selected interface $INTERFACE ($REPLY)"
-            sudo brctl addbr $TBOTS_ETHERNET_INTERFACE && \
-            sudo brctl addif $TBOTS_ETHERNET_INTERFACE $INTERFACE && \
-            sudo ip link set dev $TBOTS_ETHERNET_INTERFACE up && \
+            echo "TBOTS_ETHERNET_INTERFACE=$INTERFACE" >> ../src/.tbots_network_profile
             echo "> Ethernet Interface Configured"
             break
             ;;
     esac
 done
+
+echo "Loading Configuration..."
+source ~/.profile && echo "Done"
