591c3311533183c49677b3e2677edd4781bd9343
akhilveeraghanta <akhil.veeraghanta@outlook.com>
2020-06-24 17:54:21 -0700

    networking scripts work

--- environment_setup/setup_networking.sh
+++ environment_setup/setup_networking.sh
@@ -2,10 +2,7 @@
 # At the time of creating this script, we have team members with enp3s0f1,
 # wlp3s0, wlp7s0, eth1, eth0 and a plethora of other network interfaces 
 #
-# This script will create two virtual interfaces tbots_wifi and tbots_eth
-# and then bridge those with the appropriate physical interfaces on the users
-# system.
-#
+# This script will prompt to select the interfaces to use for tbots networking
 # This script can be rerun to reconfigure the interfaces.
 echo "================================================================"
 echo " Networking Interface Setup"
@@ -13,14 +10,11 @@ echo "================================================================"
 
 # get the "suggested" interface(s). The first interface is usually the correct
 # one but we provide the option to select other interfaces, which is useful for VM users
-SUGGESTED_INTERFACE=$(route | grep '^default' | grep -o '[^ ]*$')
+SUGGESTED_INTERFACES=$(route | grep '^default' | grep -o '[^ ]*$')
 
 PS3="Suggested interfaces(s):
-$SUGGESTED_INTERFACE
+$SUGGESTED_INTERFACES
 > Select wifi interface (input 0 to skip): "
-
-# remove previous profile if it exists
-rm ../src/.tbots_network_profile 2> /dev/null
 
 echo "> Configure WiFi interface"
 
@@ -34,7 +28,7 @@ do
         *)
             echo "> Selected interface $INTERFACE ($REPLY)"
             echo "export TBOTS_WIFI_INTERFACE=$INTERFACE" >> ~/.profile
-            echo "> WiFi Interface Configured"
+            source ~/.profile && echo "> WiFi Interface Configured"
             break
             ;;
     esac
@@ -53,13 +47,14 @@ do
         *)
             echo "> Selected interface $INTERFACE ($REPLY)"
             echo "export TBOTS_ETHERNET_INTERFACE=$INTERFACE" >> ~/.profile
-            echo "> Ethernet Interface Configured"
+            source ~/.profile && echo "> Ethernet Interface Configured"
             break
             ;;
     esac
 done
 
-echo "Loading Configuration..."
-source ~/.profile && echo "Done"
-
-echo "Be sure to cleanup duplicate entries in ~/.profile if this script has been run more than once"
+echo "================================================================"
+echo "Done. Please run source ~/.profile or restart all shell sessions"
+echo "Be sure to cleanup duplicate TBOTS_INTERFACE entries in ~/.profile 
+if this script has been run more than once"
+echo "================================================================"
